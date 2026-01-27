1f6d9937983e925b02e01fed4dc0824e1bd8852b
Sandi Baressi Šegota <ssegota@gmail.com>
2026-01-27 20:18:41 +0100

    fix: register Lambda in backend.ts for direct access to configure env vars

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,7 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { createPatientCognito } from './functions/create-patient-cognito/resource';
 import { PolicyStatement, Effect } from 'aws-cdk-lib/aws-iam';
 import { Function } from 'aws-cdk-lib/aws-lambda';
 
@@ -10,33 +11,25 @@ import { Function } from 'aws-cdk-lib/aws-lambda';
 const backend = defineBackend({
   auth,
   data,
+  createPatientCognito,
 });
 
 // Get auth resources
 const userPoolArn = backend.auth.resources.userPool.userPoolArn;
 const userPoolId = backend.auth.resources.userPool.userPoolId;
 
-// Grant the Lambda function (defined as data handler) permission to manage Cognito users
-// Iterate over all functions to find the create-patient-cognito Lambda
-const functions = backend.data.resources.functions;
-for (const [name, fn] of Object.entries(functions)) {
-  console.log('Found function:', name);
-  if (name.includes('createPatientCognito') || name.includes('create-patient-cognito')) {
-    const lambdaFn = fn as Function;
+// Get the Lambda function directly from the backend (not through data)
+const lambdaFn = backend.createPatientCognito.resources.lambda as Function;
 
-    lambdaFn.addToRolePolicy(
-      new PolicyStatement({
-        effect: Effect.ALLOW,
-        actions: [
-          'cognito-idp:AdminCreateUser',
-          'cognito-idp:AdminSetUserPassword',
-        ],
-        resources: [userPoolArn],
-      })
-    );
+lambdaFn.addToRolePolicy(
+  new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: [
+      'cognito-idp:AdminCreateUser',
+      'cognito-idp:AdminSetUserPassword',
+    ],
+    resources: [userPoolArn],
+  })
+);
 
-    lambdaFn.addEnvironment('USER_POOL_ID', userPoolId);
-    console.log('Configured Lambda:', name);
-  }
-}
-
+lambdaFn.addEnvironment('USER_POOL_ID', userPoolId);
